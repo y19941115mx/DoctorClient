d8628700d26f10b98f9544c9fa8ba19c4427053e
y19941115mx <1960892068@qq.com>
2018-02-27 15:43:25 +0800

    提交代码

--- 速递医疗 医生端/ViewController/Mine_setting.swift
+++ 速递医疗 医生端/ViewController/Mine_setting.swift
@@ -29,8 +29,8 @@ class Mine_setting: BaseTableInfoViewController{
     override func viewDidLoad() {
         super.viewDidLoad()
         tableView.tableFooterView = UIView()
-        let titles = [["意见反馈","退出登录"],["应用分享"]]
-        let infos:[[String]] = [["",""], [""]]
+        let titles = [["意见反馈","退出登录"],["版本信息","应用分享"]]
+        let infos:[[String]] = [["",""], ["", ""]]
         initViewController(tableTiles: titles, tableInfo: infos, tableView: tableView) { (indexpath) in
             self.handler(indexPath: indexpath)
         }
@@ -47,8 +47,13 @@ class Mine_setting: BaseTableInfoViewController{
     
     private func handler(indexPath:IndexPath) {
         if indexPath.section == 1 {
-            // 分享
-            umengShare()
+            if indexPath.row == 0 {
+                let vc = editionViewController()
+                self.navigationController?.pushViewController(vc, animated: false)
+            }else {
+                // 分享
+                umengShare()
+            }
         }else {
             if indexPath.row == 0 {
                 // 用户反馈界面
@@ -122,5 +127,76 @@ class FeedBackViewController:BaseViewController {
             }
         }
     }
+}
+
+// 版本相关 视图控制器
+class editionViewController:BaseViewController, UITableViewDataSource {
+    let data = ["应用名", "版本号", "版权所有"]
     
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return data.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let id = "cell"
+        var cell = tableView.dequeueReusableCell(withIdentifier: id)
+        
+        if cell == nil {
+            cell = UITableViewCell.init(style: .value2, reuseIdentifier: id)
+        }
+        
+        cell?.textLabel?.text = data[indexPath.row]
+        switch indexPath.row {
+        case 0:
+            cell?.detailTextLabel?.text = "速递医疗_医生端"
+        case 1:
+            cell?.detailTextLabel?.text = APPVERSION
+        case 2:
+            cell?.detailTextLabel?.text = "合肥顶岭医疗科技开发有限公司"
+        default:
+            cell?.detailTextLabel?.text = ""
+        }
+        
+        return cell!
+        
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        setUpNavTitle(title: "软件版本")
+        setUpView()
+    }
+    
+    private func setUpView() {
+        // 添加 titleView
+        let titleView = UIView()
+        titleView.backgroundColor = UIColor.APPColor
+        let logoView = UIImageView.init(image: #imageLiteral(resourceName: "applogo"))
+//        logoView.cornerRadius = 40
+        
+        self.view.addSubview(titleView)
+        titleView.addSubview(logoView)
+        
+        titleView.snp.makeConstraints { (make) in
+            make.left.equalTo(0)
+            make.right.equalTo(0)
+            make.top.equalTo(0)
+            make.height.equalTo(self.view).multipliedBy(0.4)
+        }
+        logoView.snp.makeConstraints { (make) in
+            make.center.equalTo(titleView)
+            make.height.width.equalTo(100)
+        }
+        // 添加 tableView
+        let tableView = BaseTableView()
+        self.view.addSubview(tableView)
+        tableView.snp.makeConstraints { (make) in
+            make.height.equalToSuperview().multipliedBy(0.6)
+            make.left.equalToSuperview()
+            make.right.equalToSuperview()
+            make.top.equalTo(titleView.snp.bottom)
+        }
+        tableView.dataSource = self
+        
+    }
 }
